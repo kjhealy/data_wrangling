d765e405902d027743f620b1d0750aaacde82ab4
Kieran Healy <kjhealy@gmail.com>
2021-09-14 18:48:22 -0400

    purl code

--- code/01-overview.R
+++ code/01-overview.R
@@ -1,7 +1,18 @@
-here::i_am("code/01-overview.R")
-
-## ----setup, include=FALSE----------------------------------------------------------------------------------------------------------------
-knitr::opts_chunk$set(warning = FALSE, message = FALSE,
+## ----setup, include=FALSE----------------------------------------------------------------------------------------------------------
+knitr::opts_chunk$set(warning = FALSE, message = FALSE, 
                       fig.retina = 3, fig.align = "center")
 
+
+## ----packages-data, include=FALSE--------------------------------------------------------------------------------------------------
+library(flipbookr)
+library(cowplot)
+ggplot2::theme_set(theme_cowplot())
+
+
+
+## ----xaringanExtra, echo=FALSE-----------------------------------------------------------------------------------------------------
+xaringanExtra::use_xaringan_extra(c("tile_view"))
+xaringanExtra::use_animate_css()
+xaringanExtra::use_animate_all("fade")
+xaringanExtra::use_clipboard()
 
