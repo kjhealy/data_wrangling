2c31e05c467a61ddf2f06031650347073cab45b2
Kieran Healy <kjhealy@gmail.com>
2023-11-18 14:01:49 -0500

    03a slides; scss tweaks

--- proc-qmd.sh
+++ proc-qmd.sh
@@ -9,3 +9,13 @@ perl -pi -e 's/\.center\[!\[:scale (\d{1,3}%) "(.*?)"\]\((.*?)\)\]/![$2]($3)\{fi
 
 ## Convert images pulled left or right
 perl -pi -e 's/\.pull-(left|right)\[!\[:scale (\d{1,3}%) "(.*?)"\]\((.*?)\)\]/![]($4)\{fig-align="center" width=$2\}/g' $1
+
+
+## Convert blank slides to huge
+
+perl -pi -e 's/# \.huge\.middle\.squish\d{1}\[(.*)]/:::{.huge}\n $1 \n:::/' $1
+
+## Remove layout: true and class: title title-1
+
+perl -pi -e 's/^layout: true//' $1
+perl -pi -e 's/^class: title title-1//' $1
