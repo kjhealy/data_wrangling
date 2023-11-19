74c19ee2a81e3573dbcef23fcfa4b8a03990b99f
Kieran Healy <kjhealy@gmail.com>
2023-11-19 15:24:41 -0500

    04 qmd

--- proc-qmd.sh
+++ proc-qmd.sh
@@ -19,3 +19,19 @@ perl -pi -e 's/# \.huge\.middle\.squish\d{1}\[(.*)]/:::{.huge}\n $1 \n:::/' $1
 
 perl -pi -e 's/^layout: true//' $1
 perl -pi -e 's/^class: title title-1//' $1
+
+# Remove class line
+perl -pi -e 's/class: center middle main-title section-title-1//' $1
+
+# Common image pattern replace
+perl -pi -e 's/\.center\[!\[:scale \d{1,3}%\](\(.*?\))\]/![]$1/' $1
+
+# Common aside
+perl -pi -e 's/\.smaller\.footnote\[(.*?)\]/::: aside\n $1\n:::/' $1
+perl -pi -e 's/\.tiny\.footnote\[(.*?)\]/::: aside\n $1\n:::/' $1
+
+
+# chunk_reveal to chunq_reveal
+perl -pi -e 's/chunk_reveal/chunq_reveal/' $1
+
+perl -pi -e 's/(chunq_reveal.*?)widths = c\((\d{1,2}),(\d{1,2})\)/$1 lcolw="$2", rcolw="$3"/' $1
