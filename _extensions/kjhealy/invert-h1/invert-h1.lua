c458427db9e60378f9dd87e7a572810cd35c6555
Kieran Healy <kjhealy@gmail.com>
2023-12-08 09:42:23 -0500

    updating ... stand by

--- _extensions/kjhealy/invert-h1/invert-h1.lua
+++
@@ -1,9 +0,0 @@
-
--- Reformat all heading text
-function Header(el)
-  if el.level == 1 then
-      table.insert(el.classes, "inv-title")
-      el.attributes["data-background-color"] = "#252F3E"
-      return el
-  end
-end
