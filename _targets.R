4bba8ab8c2b81b31ae06f3e467a3050e501fed77
Kieran Healy <kjhealy@gmail.com>
2023-12-11 18:01:16 -0500

    Finally fix deletion step

--- _targets.R
+++ _targets.R
@@ -139,20 +139,20 @@ list(
   format = "file"),
 
   ## Remove any flipbookr leftover dirs
-  # tar_files(flipbookr_dirs, {
-  #   # Force dependencies
-  #   flipbookr_orphans
-  #   # Top-level flipbookr dirs now empty
-  #   get_leftover_dirs()
-  # }
-  # ),
-  #
-  # tar_target(empty_dirs, {
-  #   remove_leftover_dirs(flipbookr_dirs)
-  # },
-  # pattern = map(flipbookr_dirs),
-  # format = "file"),
-  #
+  tar_files(flipbookr_dirs, {
+    # Force dependencies
+    quarto_pdfs
+    # Top-level flipbookr dirs now empty
+    get_leftover_dirs()
+  }
+  ),
+
+  tar_target(empty_dirs, {
+    remove_leftover_dirs(flipbookr_dirs)
+  },
+  pattern = map(flipbookr_dirs)),
+
+
 
   ## Upload site ----
   tar_target(deploy_script, here_rel("deploy.sh"), format = "file"),
