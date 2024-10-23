fadd5fc0fc7bcb280d6252c8bc43f2c1b3e949bd
Kieran Healy <kjhealy@gmail.com>
2024-10-23 08:24:10 -0400

    Add persmissions failsafe

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,11 @@ PUBLIC_DIR='_site/'
 SSH_USER='kjhealy@kjhealy.co'
 DOCUMENT_ROOT='~/public/kjhealy/public_html/dw'
 
-echo "Uploading changes to remote server..."
+echo "Setting permissions ..."
 cp html/htaccess $PUBLIC_DIR/.htaccess
+
+find $PUBLIC_DIR -type d -print0 | xargs -0 chmod 755
+find $PUBLIC_DIR -type f -print0 | xargs -0 chmod 644
+
+echo "Uploading changes to remote server..."
 rsync --exclude='.DS_Store' -Prvzce 'ssh -p 22' $PUBLIC_DIR $SSH_USER:$DOCUMENT_ROOT --delete-after
